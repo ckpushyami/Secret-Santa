9bc9fa790fb21696adfe1f806cc14dedd81b8bd2
jordandsnyder <jordandanielsnyder@gmail.com>
2019-11-24 19:01:07 -0800

    created test data frame

--- initial.R
+++ initial.R
@@ -1,2 +1,15 @@
 library(tidyverse)
 library(ponyexpress)
+
+#Create a test data
+firstnames <- c("Jack", "Jill", "Lyle", "Lilly", "Bill", "Dan", "Deloris", "Emily")
+lastnames <- c("Smith", "Smith", "Lewis", "Lewis", "Buttes", "Deznada", "Deznada", "Richardson")
+email <- c("jacks@test.com", "jills@test.com", "lylel@test.com", "lillyl@test.com", "billb@test.com",
+           "dand@test.com","delorisd@test.com", "emilyr@test.com")
+testdf <- as.data.frame(cbind(firstnames,lastnames,email))
+
+colnames(testdf) <- c("Name", "Family", "Email")
+
+
+#Secret Santa Assigner - Select someone who does not have the same family name and make sure everyone is chosen without a closed loop.
+
